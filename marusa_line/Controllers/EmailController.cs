a773d405a4e19c3286c2a9eaf3b436b275ef7c49
George <119735003+Tseretel1@users.noreply.github.com>
2026-03-01 19:43:21 +0400

    created email contoller, starting to integrate email sending system

---
+++ marusa_line/Controllers/EmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace marusa_line.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
